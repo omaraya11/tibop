2c9d4a168822ce4844ee32b03687916c29753199
Yasser Hennawi <yahennawi@gmail.com>
2017-06-23 23:39:00 +0200

    Handling loggedin state

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ window.myMap = function() {
   })
   .then((data)=>{
     var mapCanvas = document.getElementById("map");
-    var myCenter = new google.maps.LatLng(data[0].latitude,data[0].longitude); 
+    var myCenter = new google.maps.LatLng(data[0].latitude,data[0].longitude);
     var mapOptions = {
       center: myCenter,
       zoom: 12,
@@ -42,7 +42,17 @@ function setMarkers(map, markers) {
   }
 }
 
-// request.post('https://ostlerapi.herokuapp.com/api/login').promise();
+function isLogged() {
+  var logoutItem = document.getElementById("logoutItem");
+  var signItem = document.getElementById("signItem");
+  if (localStorage.getItem('token')) {
+      logoutItem.setAttribute("style", "display: flex");
+      signItem.setAttribute("style", "display: none");
+  } else {
+    signItem.setAttribute("style", "display: flex");
+    logoutItem.setAttribute("style", "display: none");
+  }
+}
 
 request.get('https://ostlerapi.herokuapp.com/api/garages').promise()
 .then( (data)=> {
@@ -52,18 +62,25 @@ request.get('https://ostlerapi.herokuapp.com/api/garages').promise()
   var select = document.getElementById("select");
   if (select) {
     for (var i = data.length - 1; i >= 0; i--) {
-      var node = document.createElement("option");  
+      var node = document.createElement("option");
       var t = document.createTextNode(data[i].name);
       node.setAttribute("value", data[i]._id);
       node.appendChild(t)
-      select.appendChild(node);
+      select.appendChild(node)
     }
-  }  
+  }
 })
 
 
 $(()=>{
+  var userDisplayName = document.getElementById('username');
+  if ( localStorage.getItem('displayName')) {
+    var t = document.createTextNode(localStorage.getItem('displayName'));
+    userDisplayName.appendChild(t);
+  }
+
   var submitButton = document.getElementById('submitBook');
+  isLogged();
   if (submitButton) {
     submitButton.onclick = ()=> {
       var date = document.getElementById('time').value;
@@ -81,15 +98,26 @@ $(()=>{
       .send(bookingObject)
       .set('Authorization', localStorage.getItem('token'))
       .then( (data)=> {
-        alert('You have booked a visit at expected arrival time '+data.body.expectedArrivalTime);
+        alert('You have booked a visit at expected arrival time ' + data.body.expectedArrivalTime);
         window.location.pathname = '/';
       }, (error)=> {
-        alert(error.res.body.message);
+        if (error.res.body.message == "Token is null") {
+          alert('You have to login to Book');
+          window.location.pathname = './login';
+        } else {
+          alert(error.res.body.message);
+        }
       })
     }
   }
 
-
+  var logoutButton = document.getElementById('logoutBtn');
+  if (logoutButton) {
+    logoutButton.onclick = ()=> {
+      localStorage.removeItem('token');
+      window.location.pathname = './login';
+    }
+  }
 
   var loginButton = document.getElementById('loginBtn');
   if (loginButton) {
@@ -105,6 +133,9 @@ $(()=>{
       .then( (data)=> {
         localStorage.setItem('userId', data.body._id );
         localStorage.setItem('token', data.body.token );
+        localStorage.setItem('displayName', data.displayName );
+
+        localStorage.setItem('displayName', data.body.displayName );
         alert('Welcome back, '+data.body.displayName);
         window.location.pathname = '/map';
       }, (error)=>{
@@ -138,6 +169,8 @@ $(()=>{
       .then((data)=>{
         localStorage.setItem('userId', data._id );
         localStorage.setItem('token', data.token );
+        localStorage.setItem('displayName', data.displayName );
+
         var user = {
           "username" : data.username ,
           "password" : data.password
